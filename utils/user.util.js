bafdb0ba4a8a387fd96bf6313f55fbfb71079511
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-03-14 17:59:25 +0400

    Transaction for deleteUser

--- utils/user.util.js
+++ utils/user.util.js
@@ -1,10 +1,13 @@
 import dbUtil from './db.util.js';
 
-// TODO: make transaction
 const deleteUser = async (userId) => {
-  await dbUtil.UserRole.destroy({ where: { user_id: userId } });
-  await dbUtil.Token.destroy({ where: { user_id: userId } });
-  await dbUtil.User.destroy({ where: { id: userId } });
+  try {
+    await dbUtil.sequelize.transaction(async (transaction) => {
+      await dbUtil.UserRole.destroy({ where: { user_id: userId } }, { transaction });
+      await dbUtil.Token.destroy({ where: { user_id: userId } }, { transaction });
+      await dbUtil.User.destroy({ where: { id: userId } }, { transaction });
+    });
+  } catch {}
 };
 
 export { deleteUser };
