3971ed6de5ce48b7ed3ea184fe5f60611e297782
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-03-15 15:04:37 +0400

    Created login route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
-import { register, registerConfirm } from '../controllers/auth.controller.js';
+import { register, registerConfirm, login } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.post('/register', register).put('/register/confirm/:confirmationToken', registerConfirm);
+router.post('/register', register).put('/register/confirm/:confirmationToken', registerConfirm).post('/login', login);
 
 export default router;
