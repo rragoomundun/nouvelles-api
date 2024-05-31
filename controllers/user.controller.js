e2ad8b6f4c641fad784423c5f73242f317dd51a3
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-05-31 12:24:05 +0400

    New route: /user/:userId

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,16 @@
 import httpStatus from 'http-status-codes';
+import { Sequelize } from 'sequelize';
 
 import asyncHandler from '../middlewares/async.middleware.js';
+
+import ErrorResponse from '../classes/errorResponse.class.js';
 
 import dbUtil from '../utils/db.util.js';
 
 /**
- * @api {GET} /user Get User
+ * @api {GET} /user Get Current User
  * @apiGroup User
- * @apiName UserGet
+ * @apiName UserGetCurrent
  *
  * @apiDescription Get the current user if authorized
  *
@@ -49,4 +52,60 @@ const getUser = asyncHandler(async (req, res, next) => {
   res.status(httpStatus.OK).json(user);
 });
 
-export { getUser };
+/**
+ * @api {GET} /user/:userId Get User Profile
+ * @apiGroup User
+ * @apiName UserGetProfile
+ *
+ * @apiDescription Get profile information of a specific user
+ *
+ * @apiParam {Number} userId The user id
+ *
+ * @apiSuccess (Success (200)) {Number} id The user's id
+ * @apiSuccess (Success (200)) {String} name The user's name
+ * @apiSuccess (Success (200)) {String} image The user's profile image
+ * @apiSuccess (Success (200)) {String} biography The user's biography
+ * @apiSuccess (Success (200)) {Number} nbDiscussions The number of discussions started
+ * @apiSuccess (Success (200)) {Number} nbMessages The number of messages posted
+ * @apiSuccess (Success (200)) {Date} registrationDate The user's registration date
+ *
+ * @apiSuccessExample Success Example
+ * {
+ *   "id": 2,
+ *   "name": "Thomas"
+ *   "image": "https://img.r3tests.net/users/2/profile-picture.png",
+ *   "biography": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+ *   "nbDiscussions": 4,
+ *   "nbMessages": "61",
+ *   "registrationDate": "2024-04-18T08:07:40.736Z"
+ * }
+ *
+ * @apiError (Error (404)) NOT_FOUND No user can be found for the specified id
+ *
+ * @apiPermission Public
+ */
+const getUserProfile = asyncHandler(async (req, res, next) => {
+  const { userId } = req.params;
+  const user = await dbUtil.User.findOne({
+    attributes: ['id', 'name', 'image', 'biography', 'registration_date'],
+    where: { id: userId },
+    raw: true
+  });
+
+  if (user === null) {
+    return next(new ErrorResponse('Cannot find user', httpStatus.NOT_FOUND, 'NOT_FOUND'));
+  }
+
+  const nbDiscussions = await dbUtil.Discussion.count({ where: { user_id: userId } });
+  const nbMessages = await dbUtil.Message.count({ where: { user_id: userId } });
+
+  user.nbDiscussions = nbDiscussions;
+  user.nbMessages = nbMessages;
+  user.registrationDate = user.registration_date;
+
+  delete user.registration_date;
+
+  res.status(httpStatus.OK).json(user);
+});
+
+export { getUser, getUserProfile };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
 
-import { getUser } from '../controllers/user.controller.js';
+import { getUser, getUserProfile } from '../controllers/user.controller.js';
 
 import { protect } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-router.get('/', protect, getUser);
+router.get('/', protect, getUser).get('/:userId', getUserProfile);
 
 export default router;
